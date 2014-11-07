ab2df48b8eb434e459c762897244e875a150f48d
Paolo Mistrangelo <p.mistrangelo@magellanoprogetti.it>
2014-11-07 17:36:19 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
-        this.scan();
+        scan();
     },
     // Bind Event Listeners
     //
